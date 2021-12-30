ffb9ea0975231ff41c851951c341a396681c64a1
Freewind <nowindlee@gmail.com>
2021-12-30 16:44:37 +0800

    simplify

--- app/src/main/java/demos/ViewModel.kt
+++ app/src/main/java/demos/ViewModel.kt
@@ -4,17 +4,13 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 
 class ViewModel : BaseObservable() {
-    private var _text: String = "change me";
-
     @Bindable
-    fun getText(): String {
-        return _text;
-    }
-
-    fun setText(newText: String) {
-        if (_text != newText) {
-            _text = newText;
+    var text: String = "change me"
+        set(newText) {
+            if (text != newText) {
+                field = newText;
+            }
+            notifyPropertyChanged(BR.text)
         }
-        notifyPropertyChanged(BR.text)
-    }
+
 }
